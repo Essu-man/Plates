fb84d63516a8dd41b0cbd7a0eef8c942a9ccacfd
Essu-man <essumanconstance514@gmail.com>
2024-12-20 16:48:59 +0000

    Commit

--- hello.py
+++ hello.py
@@ -10,5 +10,9 @@ def hello_world():
 def login():
     return render_template("login.html")
 
+@app.route("/Dashboard")
+def login():
+    return render_template("Dashboard")
+
 if __name__ == "__main__":
     app.run(debug=True)
